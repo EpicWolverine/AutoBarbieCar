4dd3a9368ec50acc4109b9f8949f3162684ab168
Wispio <olesh@Oleshs-MacBook-Pro-3.local>
2015-05-18 16:29:46 -0400

    Initial Motor Shield Implementation

--- AutoBarbieCar.ino
+++ AutoBarbieCar.ino
@@ -1,6 +1,6 @@
 /* Servo */
-#include <Servo.h> 
-Servo turnServo;  //create servo object to control a servo 
+//#include <Servo.h> 
+//Servo turnServo;  //create servo object to control a servo 
 
 /* Compass */
 #include <Average.h> //import Average library for compass initialization
@@ -12,11 +12,14 @@ const float Pi = 3.14159; //apparently the pi constant isn't built into Arduino
 float compassStraight = 0.0; //this will hold what we decide as "straight"
 
 /* Turning */
+/*
 const byte turnServoPin = 9; //pin for servo motor
 byte turnServoDirection = 1; //0=left; 1=center; 2=right
 //byte turnServoLastDirection = 1;
+*/
 
 /* Drive Motor */
+/*
 const byte driveMotorDirectionPin = 4; //pin for drive motor direction control
 const byte driveMotorSpeedPin = 5; //pin for drive motor speed control
 //unsigned long driveMotorUpdateDuration; //will store last time motor was updated
@@ -24,6 +27,19 @@ unsigned long driveMotorPreviousMillis; //will store last time motor was updated
 int driveMotorDelay; //how long to wait before updating the motor during manuver
 boolean driveMotorDirection = false; //false=forward; true=backward
 //boolean driveMotorLastDirection = false;
+*/
+
+/* Monster Motor Sheild */
+#define BRAKEVCC 0
+#define CW   1 //Clockwise Arguement in motorGo
+#define CCW  2 //Counterclockwise Arguement in motorGo
+#define BRAKEGND 3
+#define CS_THRESHOLD 100
+int inApin[2] = {7, 4};
+int inBpin[2] = {8, 9};
+int pwmpin[2] = {5, 6};
+unsigned long driveMotorPreviousMillis; //will store last time motor was updated
+int driveMotorDelay; //how long to wait before updating the motor during manuver
 
 /* Distance Sensors */
 int FLDistSensorVal;          //value from the distance Front Left sensor
@@ -45,11 +61,22 @@ void setup(){
     //set pins
     pinMode(FLDistSensorPin, INPUT);
     pinMode(FRDistSensorPin, INPUT);
-    pinMode(10, OUTPUT); //left LED (red)
-    pinMode(11, OUTPUT); //center LED (yellow)
-    pinMode(12, OUTPUT); //right LED (green)
-    pinMode(driveMotorDirectionPin, OUTPUT); 
-    turnServo.attach(turnServoPin);  //attaches the servo to the servo object
+    //pinMode(10, OUTPUT); //left LED (red)
+    //pinMode(11, OUTPUT); //center LED (yellow)
+    //pinMode(12, OUTPUT); //right LED (green)
+    //pinMode(driveMotorDirectionPin, OUTPUT); 
+    //turnServo.attach(turnServoPin);  //attaches the servo to the servo object
+    for (int i=0; i<2; i++){
+      pinMode(inApin[i], OUTPUT);
+      pinMode(inBpin[i], OUTPUT);
+      pinMode(pwmpin[i], OUTPUT);
+    }
+    
+    //initilize
+    for (int i=0; i<2; i++){ //initilize motors, not running
+      digitalWrite(inApin[i], LOW);
+      digitalWrite(inBpin[i], LOW);
+    }
     if(!mag.begin()){ //initilize compass
         //There was a problem detecting the LSM303 ... check your connections
         Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
@@ -75,8 +102,8 @@ void loop(){
     Serial.println("--");
     
     //check for distance sensor detection
-    if(FLDistSensorVal < distThreshold){FLDetect=true; digitalWrite(10,HIGH);}
-    if(FRDistSensorVal < distThreshold){FRDetect=true; digitalWrite(12,HIGH);}
+    if(FLDistSensorVal < distThreshold){FLDetect=true;}
+    if(FRDistSensorVal < distThreshold){FRDetect=true;}
     
     //check if we need to override a manuver
     if (FLDetect==true && FRDetect==true){ //both
@@ -110,12 +137,12 @@ void loop(){
             driveMotorDelay = 2000;
         }
         else if(FLDetect==true){  //left
-            backward(150);
+            //backward(150);
             left();
             driveMotorDelay = 2000;
         }
         else if(FRDetect==true){  //right
-            backward(150);
+            //backward(150);
             right();
             driveMotorDelay = 2000;
         }
@@ -125,46 +152,56 @@ void loop(){
     FLDetect=false;
     FRDetect=false;
     //reset distance sensor detect LEDs
-    digitalWrite(10,LOW);
-    digitalWrite(11,LOW);
-    digitalWrite(12,LOW);
+    //digitalWrite(10,LOW);
+    //digitalWrite(11,LOW);
+    //digitalWrite(12,LOW);
 }
+
+
 
 /* Basic Manuver Functions */
 void forward(int velocity){ //drive forward
-    digitalWrite(driveMotorDirectionPin, LOW); //LOW is forward
-    analogWrite(driveMotorSpeedPin, velocity);
-    driveMotorDirection = false;
+    motorGo(0, CW, 1023);
+    motorGo(1, CCW, 1023);
+    //digitalWrite(driveMotorDirectionPin, LOW); //LOW is forward
+    //analogWrite(driveMotorSpeedPin, velocity);
+    //driveMotorDirection = false;
     //driveMotorLastDirection = false;
     //Serial.println("forward");
 }
 
 void backward(int velocity){ //drive backward
     delay(50);
-    digitalWrite(driveMotorDirectionPin, HIGH); //HIGH is backward
-    analogWrite(driveMotorSpeedPin, velocity);
-    driveMotorDirection = true;
+    motorGo(0, CCW, 1023);
+    motorGo(1, CW, 1023);
+    //digitalWrite(driveMotorDirectionPin, HIGH); //HIGH is backward
+    //analogWrite(driveMotorSpeedPin, velocity);
+    //driveMotorDirection = true;
     //driveMotorLastDirection = true;
     //Serial.println("backward");
 }
 
 void straight(){ //turn wheels straight
-    turnServo.write(79);  //turn servo to x degrees
-    turnServoDirection = 1;
+    //turnServo.write(79);  //turn servo to x degrees
+    //turnServoDirection = 1;
     //turnServoLastDirection = 1
     //Serial.println("straight");
 }
 
 void left(){ //turn wheels left
-    turnServo.write(66); //turn servo to x degrees
-    turnServoDirection = 0;
+    motorGo(0, CW, 1023);
+    motorGo(1, CW, 1023);
+    //turnServo.write(66); //turn servo to x degrees
+    //turnServoDirection = 0;
     //turnServoLastDirection = 0;
     //Serial.println("left");
 }
 
 void right(){ //turn wheels right
-    turnServo.write(89); //turn servo to x degrees
-    turnServoDirection  = 2;
+    motorGo(0, CCW, 1023);
+    motorGo(1, CCW, 1023);
+    //turnServo.write(89); //turn servo to x degrees
+    //turnServoDirection  = 2;
     //turnServoLastDirection = 2;
     //Serial.println("right");
 }
@@ -243,4 +280,38 @@ long readSensorCorrected(byte pwPin, long pulse){
         pulsetemp = readSensorRaw(pwPin);
     }
     return pulsetemp;
+}
+/* Motor Control Funtions */
+void motorGo(uint8_t motor, uint8_t direct, uint8_t pwm)
+{
+  if (motor <= 1) //Only 2 pins on sheild (Read as "0" and "1"). Makes sure you didn't invent a third one
+  {
+    if (direct <=4)
+    {
+      // Set inA[motor]
+      if (direct <=1)
+        digitalWrite(inApin[motor], HIGH);
+      else
+        digitalWrite(inApin[motor], LOW);
+
+      // Set inB[motor]
+      if ((direct==0)||(direct==2))
+        digitalWrite(inBpin[motor], HIGH);
+      else
+        digitalWrite(inBpin[motor], LOW);
+
+      analogWrite(pwmpin[motor], pwm); // Speed/Strength/Power of motor (? to 1023)
+    }
+  }
+}
+
+void motorOff(int motor) //Choose the motor to turn off, 0 or 1.
+{
+  // Initialize braked
+  for (int i=0; i<2; i++)//This just turns EVERYTHING off.
+  {
+    digitalWrite(inApin[i], LOW);
+    digitalWrite(inBpin[i], LOW);
+  }
+  analogWrite(pwmpin[motor], 0);
 }
